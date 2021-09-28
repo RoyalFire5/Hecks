c4fcbcfec5b22854ac77148857346c94528f6799
RoyalFire5 <74427239+RoyalFire5@users.noreply.github.com>
2021-09-27 20:42:50 -0400

    Delete cookieClicker directory

--- cookieClicker/cookieHecker.js
+++
@@ -1,8 +0,0 @@
-heckScript = 'heck({{ COOKIES }}, {{ LUMPS }});function heck(heckCookies, heckLumps) {if (window.location.href.indexOf("https://orteil.dashnet.org/cookieclicker/") > -1) {console.log("Hecking " + heckCookies + " cookies...");cookieHeck(heckCookies);console.log("Hecked " + heckCookies + " cookies!");console.log("Hecking " + heckLumps + " lumps
-
-function getHeckscript(cookies, lumps) {
-	heckScript.replace("{{ COOKIES }}", cookies);
-	heckScript.replace("{{ LUMPS }}", lumps);
-	
-	navigator.clipboard.writeText(heckScript);
-}
